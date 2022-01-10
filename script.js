0c4bd5944ce1fc2b85164a4a03c5baefb8425f89
Glitch (glitch-blank-website) <none>
2022-01-10 10:16:35 +0000

    💒🏵 Checkpoint ./script.js:32506642/1194 ./index.html:32506642/5498 ./style.css:32506642/6

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-const sections = document.querySelectorAll('section');
-const navLi = document.querySelectorAll('nav a');
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll("nav a");
 
-
-window.addEventListener('scroll', ()=> {
+window.addEventListener("scroll", () => {
   let current = "";
-  
-  sections.forEach( section => {
+
+  sections.forEach((section) => {
     const sectionTop = section.offsetTop;
     const sectionHeight = section.clientHeight;
-    if(pageYOffset >= sectionTop){
-      current = section.getAttribute('class');
+    if (pageYOffset >= sectionTop) {
+      current = section.getAttribute("id");
     }
-  })
-  
-  navLi.forEach( a)
-})
+  });
+
+  navLi.forEach((a) => {
+    a.classList.remove("active");
+    if (a.classList.contains(current)) {
+      a.classList.add("active");
+    }
+  });
+});
