6338400ddcbac5f39016f82c52a90281f7ed09eb
Glitch (glitch-blank-website) <none>
2022-01-10 10:04:54 +0000

    🏬💂 Checkpoint ./script.js:32506642/423

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 const sections = document.querySelectorAll('section');
 const navLi = document.querySelectorAll('nav a');
+
+
+window.addEventListener('scroll', ()=> {
+  let current = "";
+  
+  sections.forEach( section => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if(pageYOffset >= sectionTop){
+      current = section.getAttribute('class');
+    }
+  })
+  
+  navLi.forEach( a)
+})
