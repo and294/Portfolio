253d697b423d8f12f074a3c414f6dab50da2981b
Glitch (speckled-bubble-tie) <none>
2022-01-20 10:39:37 +0000

    🦃🍍 Checkpoint ./script.js:32506642/303

--- script.js
+++ script.js
@@ -39,3 +39,17 @@ navToggle.addEventListener('click', () => {
 /*pres text roll in on scroll*/
 const text = document.querySelector('#cvPres');
 
+let options = {
+  rootMargin: '-10%',
+  threshold: 0.0
+};
+
+let observer = new IntersectionObserver(showItem, options);
+
+function showItem(entries) {
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      entry.target.children[0].classList.add('active')
+    }
+  })
+}
