5748ef525523eacc4ee16818d271aa61090dbd26
Glitch (speckled-bubble-tie) <none>
2022-01-21 10:31:22 +0000

    🏈🚴 Checkpoint ./script.js:32506642/6394

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 /*Highlight section when scrolled by*/
 const sections = document.querySelectorAll("section");
 const navLi = document.querySelectorAll("nav a");
@@ -9,7 +8,7 @@ window.addEventListener("scroll", () => {
   sections.forEach((section) => {
     const sectionTop = section.offsetTop;
     const sectionHeight = section.clientHeight;
-    if (pageYOffset >= (sectionTop - sectionHeight / 3)) {
+    if (pageYOffset >= sectionTop - sectionHeight / 3) {
       current = section.getAttribute("id");
     }
   });
@@ -23,61 +22,89 @@ window.addEventListener("scroll", () => {
 });
 
 /*hide nav on phone and tablet*/
-const primaryNav = document.querySelector('.nav');
-const navToggle = document.querySelector('.mobile-nav-toggle');
+const primaryNav = document.querySelector(".nav");
+const navToggle = document.querySelector(".mobile-nav-toggle");
 
-navToggle.addEventListener('click', () => {
+navToggle.addEventListener("click", () => {
   const visibility = primaryNav.getAttribute("data-visible");
-  
-  if(visibility === "false") {
-    primaryNav.setAttribute("data-visible", "true")
+
+  if (visibility === "false") {
+    primaryNav.setAttribute("data-visible", "true");
   } else {
-    primaryNav.setAttribute("data-visible", "false")
+    primaryNav.setAttribute("data-visible", "false");
   }
-})
-
-
-
+});
 
 /*Observer animations*/
 
 const config = {
     root: null, // Sets the framing element to the viewport
-    rootMargin: "0px",
-    threshold: 0.5
+    rootMargin: "50px",
+    threshold: 0.5,
   },
- 
-   /*Who am I*/   
-      
-   steps = document.querySelectorAll(".steps"),
-  observerSteps = new IntersectionObserver((entries) => entries
-    .forEach(({target: {classList}, intersectionRatio}) => {
-      if(intersectionRatio > 0.5){
+  /*Who am I*/
+  step1 = document.querySelector(".step1"),
+  step2 = document.querySelector(".step2"),
+  step3 = document.querySelector(".step3"),
+  observerStep1 = new IntersectionObserver(
+    (entries) =>
+      entries.forEach(({ target: { classList }, intersectionRatio }) => {
+        if (intersectionRatio > 0.5) {
+          classList.add("stepsIn");
+        } else {
+          classList.remove("stepsIn");
+        }
+      }),
+    config
+  );
+
+observerStep1.observe(step1);
+
+observerStep2 = new IntersectionObserver(
+  (entries) =>
+    entries.forEach(({ target: { classList }, intersectionRatio }) => {
+      if (intersectionRatio > 0.5) {
         classList.add("stepsIn");
-      }
-      else{
+      } else {
         classList.remove("stepsIn");
       }
-    }), config);
+    }),
+  config
+);
 
-observerSteps.observe(steps);   
-      
-      
- /*Bio roll in*/
+observerStep1.observe(step2);
+
+observerStep3 = new IntersectionObserver(
+  (entries) =>
+    entries.forEach(({ target: { classList }, intersectionRatio }) => {
+      if (intersectionRatio > 0.5) {
+        classList.add("stepsIn");
+      } else {
+        classList.remove("stepsIn");
+      }
+    }),
+  config
+);
+
+observerStep1.observe(step3);
+
+/*Bio roll in*/
 const configBio = {
     root: null, // Sets the framing element to the viewport
     rootMargin: "0px",
-    threshold: 0.5
+    threshold: 0.5,
   },
   bio = document.querySelector(".bio"),
-  observerBio = new IntersectionObserver((entries) => entries
-    .forEach(({target: {classList}, intersectionRatio}) => {
-      if(intersectionRatio > 0.5){
-        classList.add("actived");
-      }
-      else{
-        classList.remove("actived");
-      }
-    }), configBio);
+  observerBio = new IntersectionObserver(
+    (entries) =>
+      entries.forEach(({ target: { classList }, intersectionRatio }) => {
+        if (intersectionRatio > 0.5) {
+          classList.add("actived");
+        } else {
+          classList.remove("actived");
+        }
+      }),
+    configBio
+  );
 
 observerBio.observe(bio);
