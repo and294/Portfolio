6323de87d8646df9eda2919b30330ab59605e134
Glitch (speckled-bubble-tie) <none>
2022-01-25 10:46:04 +0000

    🍤👻 Checkpoint ./script.js:32506642/634 ./index.html:32506642/12

--- script.js
+++ script.js
@@ -90,3 +90,21 @@ observerBio.observe(bio);
   duration: 3,
   stager: 0.1
 })*/
+
+const configBar = {
+    root: null, // Sets the framing element to the viewport
+    rootMargin: "0px",
+    threshold: 0.5,
+  },
+  etiquette = document.querySelectorAll(".bar abbr"),
+  observerEtiquette = new IntersectionObserver(
+    (entries) =>
+      entries.forEach(({ target: { classList }, intersectionRatio }) => {
+        if (intersectionRatio > 0.5) {
+          classList.add("abbrActived");
+        }
+      }),
+    configBio
+  );
+
+observerBio.observe(etiquette);
