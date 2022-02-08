6f49f89302b6411f36f08e5a3bf01e7c7edc9982
Glitch (speckled-bubble-tie) <none>
2022-02-08 10:37:01 +0000

    🐔🍉 Checkpoint ./script.js:32506642/291 ./index.html:32506642/102

--- script.js
+++ script.js
@@ -38,6 +38,22 @@ navToggle.addEventListener("click", () => {
   }
 });
 
+gsap.to(".panel:not(:last-child)", {
+  yPercent: -100, 
+  ease: "none",
+  stagger: 0.5,
+  scrollTrigger: {
+    trigger: "#container",
+    start: "top top",
+    end: "+=300%",
+    scrub: true,
+    pin: true
+  }
+});
+
+
+gsap.set(".panel", {zIndex: (i, target, targets) => targets.length - i});
+
 /*Hello swing on load*/
 const swingRadius = gsap.getProperty("body", "--radius"); // gets the radius var from CSS... useful for this example's flexability.
 
