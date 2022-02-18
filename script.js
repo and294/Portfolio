2d0cd892e6c845e492216de3437642d6f0c10572
Glitch (speckled-bubble-tie) <none>
2022-02-18 15:00:46 +0000

    🐗👡 Checkpoint ./script.js:32506642/2557 ./index.html:32506642/680

--- script.js
+++ script.js
@@ -43,21 +43,6 @@ navToggle.addEventListener("click", () => {
   }
 });
 
-/*scroll transition*/
-/*gsap.to(".panel:not(:last-child)", {
-  yPercent: -100, 
-  ease: "none",
-  stagger: 0.5,
-  scrollTrigger: {
-    trigger: "#container",
-    start: "top top",
-    end: "+=300%",
-    scrub: true,
-    pin: true
-  }
-});
-
-gsap.set(".panel", {zIndex: (i, target, targets) => targets.length - i});*/
 
 /*little bar animation*/
 
