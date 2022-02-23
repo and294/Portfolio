32bfe0b62f4c9af16fd75ca006333207df29b04d
Glitch (speckled-bubble-tie) <none>
2022-02-23 10:46:08 +0000

    🐧🍄 Checkpoint ./style.css:32506642/27 ./contactForm.html:32506642/75 ./script.js:32506642/451

--- script.js
+++ script.js
@@ -300,3 +300,20 @@ gsap.from(".tile", {
   opacity: 0,
   stager: { eact: 0.5 },
 });
+
+/*Copy Phone to clipboard*/
+
+function copyPhoneNumber() {
+  /* Get the text field */
+  var copyText = document.getElementById("myInput");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+  /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+  
+  /* Alert the copied text */
+  alert("Copied the text: " + copyText.value);
+}
