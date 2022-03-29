64cfcdcddc6ec862f582cd4bb59573626b3fcd8e
Glitch (my-portfolio-and29) <none>
2022-03-29 12:17:09 +0000

    Major update

--- script.js
+++ script.js
@@ -81,27 +81,6 @@ gsap.from(".hr3", {
   duration: 1,
 });
 
-/*Intro div animation on load*/
-gsap.from(".intro", {
-  scrollTrigger: ".intro",
-  autoAlpha: 0,
-  height: 1000,
-  ease: Power2.easeInOut,
-  duration: 1,
-  stager: 0.1,
-});
-
-
-/*Photo scale*/
-gsap.from(".moi", {
-  scrollTrigger: ".moi",
-  autoAlpha: 0,
-  scale: 1.5,
-  y: -10,
-  ease: Power2.easeInOut,
-  duration: 1.5,
-  stager: 0.1,
-});
 
 /*Hello swing on load*/
 const swingRadius = gsap.getProperty("body", "--radius"); // gets the radius var from CSS... useful for this example's flexability.
@@ -109,80 +88,93 @@ const swingRadius = gsap.getProperty("body", "--radius"); // gets the radius var
 const transformOriginValue = "50% -" + swingRadius + "vh";
 
 gsap.from(".hello", {
-  duration: 10,
+  duration: 7,
   rotation: "-30deg",
   transformOrigin: transformOriginValue,
   ease: "elastic.out( 3, 0.1)",
-  /*repeat: -1,*/
-  delay: 0,
-});
+  opacity: 0,
+})
+
+//Landing page timeline
+
+const navElements = document.querySelectorAll(".navItem")
+
+const introTL = gsap.timeline();
+
+introTL
+  .from(".intro", {
+  opacity: 0,
+  autoAlpha: 0,
+  height: 1000,
+  ease: "power2.easeInOut",
+  duration: 1,
+})
 
 /*Name slide in*/
-/*var rule = CSSRulePlugin.getRule(".name:after");*/
-
-gsap.from(".name", {
-  scrollTrigger: ".name",
+.from(".name", {
   opacity: 0,
   y: -50,
-  duration: 1.5,
-});
+  duration: 0.5,
+ 
+})
 
-/*gsap.to(rule, { cssRule: { scaleY: 0 }, duration: 1 });
-*/
 /*Font end slide in*/
-gsap.from(".frontend", {
-  scrollTrigger: ".frontend",
+.from(".frontend", {
   autoAlpha: 0,
-  xPercent: 20,
-  ease: Power2.easeInOut,
-  duration: 1,
-  delay: 0.5,
-});
-/*photo roll in*/
-gsap.from(".photo", {
-  scrollTrigger: ".photo",
-  autoAlpha: 0,
-  xPercent: -100,
-  ease: Power2.easeInOut,
-  rotation: -360,
-  duration: 3,
-  stager: 0.1,
-});
+  //xPercent: 20,
+  y: -50,
+  ease: "power3.out",
+  duration: 0.5,
+  delay: 0.1,
+})
 
 /*Scroll down Arrow*/
-gsap.from(".arrow", {
-  scrollTrigger: ".arrow",
+.from(".arrow", {
   autoAlpha: 0,
   yPercent: -100,
-  ease: Power2.easeInOut,
-  delay: 1.5,
-  duration: 1,
-  stager: 0.1,
+  ease: "power2.easeInOut",
+  delay: 0.3,
+  duration: 0.5,
+})
+.from(navElements, {
+  opacity: 0,
+  y: 40,
+  ease: "power2.easeInOut",
+  duration: 0.5,
+  stagger: {
+    amount: 0.8
+  }
 });
+
+const whoAmITL = gsap.timeline();
+
 /*Who am I slide in*/
-gsap.from(".propo", {
-  scrollTrigger: ".propo",
-  autoAlpha: 0,
+
+//whoAmITL
+ gsap.from(".propo", {
+  scrollTrigger:{
+  trigger: ".propo",
+  start: "top 99%"
+  },
+  opacity: 0,
   yPercent: 50,
   ease: Power2.easeInOut,
   duration: 1,
-  stager: 0.1,
-});
+})
 
 /*bio slide in*/
 gsap.from(".bio", {
-  scrollTrigger: ".bio",
+  scrollTrigger:{
+  trigger: ".bio",
+  start: "top 99%"
+  },
   autoAlpha: 0,
   xPercent: 50,
   ease: Power2.easeInOut,
   duration: 1,
   delay: 0.5,
   stager: 0.1,
-});
-/*Tilt effect on bio*/
-VanillaTilt.init(document.querySelector(".bio"), {
-  glare: true,
-});
+})
 
 /*What can I do slide in*/
 gsap.from(".service", {
@@ -195,7 +187,10 @@ gsap.from(".service", {
   duration: 1,
 });
 
-
+/*Tilt effect on bio*/
+VanillaTilt.init(document.querySelector(".bio"), {
+  glare: true,
+});
 
 /*Parallax effect on services*/
 
@@ -211,7 +206,7 @@ gsap.from(".service", {
 gsap.from(".discoverEntrance", {
   scrollTrigger: {
     trigger: ".discoverEntrance",
-    start: "top 90%",
+    start: "top 99%",
   },
   autoAlpha: 0,
   opacity: 0,
@@ -225,7 +220,7 @@ gsap.from(".discoverEntrance", {
 gsap.from(".designEntrance", {
   scrollTrigger: {
     trigger: ".designEntrance",
-    start: "top 80%",
+    start: "top 99%",
   },
   autoAlpha: 0,
   opacity: 0,
@@ -239,7 +234,7 @@ gsap.from(".designEntrance", {
 gsap.from(".codeEntrance", {
   scrollTrigger: {
     trigger: ".codeEntrance",
-    start: "top 70%",
+    start: "top 99%",
   },
   autoAlpha: 0,
   opacity: 0,
@@ -253,28 +248,28 @@ gsap.from(".codeEntrance", {
 gsap.from(".skill", {
   scrollTrigger: {
     trigger: ".skill",
-    start: "top 90%",
+    start: "top 99%",
   },
   opacity: 0,
   y: 50,
   duration: 1,
 });
 /*Skill bars animation*/
-let bars = document.querySelectorAll(".barEntrance");
+const bars = document.querySelectorAll(".bar");
 
 gsap.from(bars, {
-  scrollTrigger: {
+ scrollTrigger: {
     trigger: ".bar",
     start: "top 90%",
   },
   y: 15,
   opacity: 0,
-  ease: Power2.easeInOut,
+  ease: "power3.out",
   duration: 1,
-  stager: { each: 0.9 },
+  stagger: { amount: 0.9 },
 });
 
-gsap.from(".bar abbr", {
+/*gsap.from(".bar abbr", {
   scrollTrigger: ".bar",
   left: -15,
   ease: Power2.easeInOut,
@@ -288,13 +283,23 @@ gsap.from(".bar span", {
   ease: Power2.easeInOut,
   duration: 3,
   stager: 0.1,
-});
+});*/
 
 /*Project slide in*/
 gsap.from(".project", {
   scrollTrigger: {
     trigger: ".project",
-    start: "top 90%",
+    start: "top 99%",
+  },
+  opacity: 0,
+  y: 50,
+  duration: 1,
+});
+
+gsap.from(".appTitle", {
+  scrollTrigger: {
+    trigger: ".appTitle",
+    start: "top 99%",
   },
   opacity: 0,
   y: 50,
@@ -302,18 +307,42 @@ gsap.from(".project", {
 });
 
 /*Grid entrance*/
+const tile = document.querySelectorAll(".tile")
 
-gsap.from(".tile", {
+gsap.from(tile, {
   scrollTrigger: {
     trigger: ".tile",
     toggleActions: "restart none none none",
-    start: "top 80%",
+    start: "top 95%",
   },
   y: -50,
   scale: 1.1,
   duration: 0.5,
   opacity: 0,
-  stager: { eact: 0.5 },
+  stagger: { eact: 0.5 },
+});
+
+gsap.from(".webTitle", {
+  scrollTrigger: {
+    trigger: ".webTitle",
+    start: "top 99%",
+  },
+  opacity: 0,
+  y: 50,
+  duration: 1,
+});
+
+gsap.from(".webImg", {
+  scrollTrigger: {
+    trigger: ".webImg",
+    toggleActions: "restart none none none",
+    start: "top 95%",
+  },
+  y: 50,
+  scale: 1.1,
+  duration: 0.5,
+  opacity: 0,
+  stagger: { eact: 0.5 },
 });
 
 /*parallax get in touch section*/
